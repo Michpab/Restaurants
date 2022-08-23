0071ec9ab5a9d88a41ea8af04a1dced85416512c
Michpab <michpab11@yahoo.com>
2022-08-23 04:53:56 -0400

    navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,27 +1,28 @@
- const React = require('react')
+const React = require('react')
 
- function Def (html) {
-     return (
-         <html>
-             <head>
-                 <title>Title</title>
-                
-                 <link rel="stylesheet" href="/css/style.css"/>
-                 {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> */}
-             </head>
+function Def (html) {
+    return (
+        <html>
+            <head>...</head>
             <body>
-                 {html.children}
-             </body>
-     </html>
-     )
- }
+                
+<nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/places">Places</a>
+    </li>
+    <li>
+      <a href="/places/new">Add Place</a>
+    </li>
+  </ul>
+</nav>
+{html.children}
+            </body>
+        </html>
+    )
+}
 
- module.exports = Def
-
-
-
-
-
-
-
-
+module.exports = Def
